b7b30c24688ea1dd6d8c9bbc2b13ec897a02f0bd
phantomfreeJr <vdavis@siu.edu>
2017-09-27 14:57:56 -0500

    ui init

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <solardawn.h>
+
+int main() {
+  init_net();
+  init_ui();
+  init_hardware();
+}
---
+++ src/solardawn.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+
+void init_net() {
+
+}
+
+void init_ui() {
+
+}
+
+void init_hardware() {
+
+}
